e17200e93c1e6f5f1c81cf793a42e01bbd89beb5
Jayesh Wadhe <jwadhe46@gmail.com>
2024-02-01 19:06:29 +0530

    initialized

---
+++ src/constants/index.js
@@ -0,0 +1,31 @@
+import { createCampaign, dashboard, logout, profile, withdraw } from '../assets';
+
+export const navlinks = [
+  {
+    name: 'dashboard',
+    imgUrl: dashboard,
+    link: '/',
+  },
+  {
+    name: 'campaign',
+    imgUrl: createCampaign,
+    link: '/create-campaign',
+  },
+  {
+    name: 'withdraw',
+    imgUrl: withdraw,
+    link: '/',
+    disabled: true,
+  },
+  {
+    name: 'profile',
+    imgUrl: profile,
+    link: '/profile',
+  },
+  {
+    name: 'disConnect',
+    imgUrl: logout,
+    link: '/',
+    disabled: true,
+  },
+];

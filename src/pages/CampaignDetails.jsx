11378c8b19bfed618a278c2c61f53ff3dc8b92ef
Jayesh Wadhe <jwadhe46@gmail.com>
2024-03-16 22:02:34 +0530

    chore : donators list updated

--- src/pages/CampaignDetails.jsx
+++ src/pages/CampaignDetails.jsx
@@ -151,10 +151,10 @@ const CampaignDetails = () => {
                     key={`${item.donator}-${index}`}
                     className="flex justify-between items-center gap-4"
                   >
-                    <p className="font-epilogue font-normal text-[16px] text-[#b2b3bd] leading-[26px] break-ll">
+                    <p className="font-epilogue font-normal text-xs md:text-[16px] text-[#b2b3bd] leading-[26px] break-ll">
                       {index + 1}. {item.donator}
                     </p>
-                    <p className="font-epilogue font-normal text-[16px] text-[#808191] leading-[26px] break-ll">
+                    <p className="font-epilogue font-normal text-sm md:text-[16px] text-[#808191] leading-[26px] break-ll">
                       {item.donation}
                     </p>
                   </div>
@@ -187,7 +187,7 @@ const CampaignDetails = () => {
             </p>
             <div className="mt-[30px]">
               <input
-                type="number"
+                type="text"
                 placeholder="ETH 0.1"
                 step="0.01"
                 className="w-full py-[10px] sm:px-[20px] px-[15px] outline-none border-[1px] border-[#3a3a43] bg-transparent font-epilogue text-white text-[18px] leading-[30px] placeholder:text-[#4b5264] rounded-[10px]"
